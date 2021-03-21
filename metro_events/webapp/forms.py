2685b4d4d16990cc8a009ab5cfdff90418ab644a
genjionn <genjionn@gmail.com>
2021-03-22 00:04:07 +0800

    test

--- metro_events/webapp/forms.py
+++ metro_events/webapp/forms.py
@@ -14,3 +14,9 @@ class LoginForm(forms.ModelForm):
    class Meta:
         model = User
         fields = ('username','password')
+
+class RequestForm(forms.ModelForm):
+
+	class Meta:
+		model = Request
+		fields = ('')

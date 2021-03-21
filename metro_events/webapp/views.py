567eda0dbed6b8bd2dee2f68cb08ad86ee9d2eec
reydominictome <70247995+reydominictome@users.noreply.github.com>
2021-03-21 21:53:10 +0800

    Register is Working

--- metro_events/webapp/views.py
+++ metro_events/webapp/views.py
@@ -21,44 +21,60 @@ class MetroEventsIndexView(View):
                 form = RegistrationForm(request.POST)	
                 data = request.POST
 
-                if form.password == form.cpassword:
+                password = data.get("password")
+                cpassword = data.get("confirm_password")
+
+                if password == cpassword:
+                    email = data.get("email")
                     user = User.objects.all()
                     count = 0
 
-                    for user in user:
-                        if(user.email == form.email):
+                    for u in user:
+                        if u.email == email:
                             count = 1
 
                     if count == 0:
-                        if form.is_valid():
-                            password = data.get("password")
-                            cpassword = data.get("cpassword")
-                            username = data.get("username")
-                            firstname = data.get("fname")
-                            midname = data.get("mname")
-                            lastname = data.get("lname")
-                            email = data.get("email") 
+                        username = data.get("username")
+                        username_count = 0
+                        for u in user:
+                            if u.username == username:
+                                username_count = 1
+                        
+                        if username_count == 0:
+                            if form.is_valid():
+                                firstname = data.get("first_name")
+                                midname = data.get("middle_name")
+                                lastname = data.get("last_name") 
 
-                            form = User(first_name = firstname, middle_name = midname, last_name = lastname, 
+                                form = User(first_name = firstname, middle_name = midname, last_name = lastname, 
                                     email = email, password = password)   
-                            form.save()
-                            form.password = make_password(form.password)
-                            form.save()
+                                form.save()
+                                form.password = make_password(form.password)
+                                form.save()
 
-                            messages.success(request, '<b>' + username + '</b> was registered successfully!')
-                            return redirect('webapp:landing')
+                                messages.success(request, '<b>' + username + '</b> was registered successfully!')
+                                return redirect('webapp:landing')
+                            
+                        print("username")    
+                        messages.success(request, '<b>' + username + '</b> is in use!')
+                        return redirect('webapp:home')
 
+                    print("Email")
                     messages.success(request, '<b>' + email + '</b> is in use!')
-                    return redirect('webapp:landing')
+                    return redirect('webapp:home')
 
+                print(password)
+                print(cpassword)
                 messages.success(request, 'Please make sure that the passwords are the same')
-                return redirect('webapp:landing')
+                return redirect('webapp:home')
 
+            print("bUTTON")
             messages.success(request, 'Something went terribly wrong')
-            return redirect('webapp:landing')
+            return redirect('webapp:home')
         else:
+            print("Rerquest")
             messages.success(request, 'Something went terribly wrong')
-            return redirect('webapp:landing')
+            return redirect('webapp:home')
 
 class MetroEventsHomeView(View):
     def get(self, request):
